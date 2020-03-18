e5faf74852c7a0f1ca6250c39432b8175d2c754c
Abdiel Ortega <nathanabdiel1@gmail.com>
2020-03-18 14:07:35 -0600

    The history configuration was added and the router is ready to work with the server

--- src/server/server.js
+++ src/server/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import webpack from 'webpack';
 
 dotenv.config();
 
@@ -8,10 +9,31 @@ const app = express();
 
 if (ENV === 'development') {
   console.log('Development config');
+  const webpackConfig = require('../../webpack.config');
+  const webpackDevMiddleware = require('webpack-dev-middleware');
+  const webpackHotMiddleware = require('webpack-hot-middleware');
+  const compiler = webpack(webpackConfig);
+  const serverConfig = { port: PORT, hot: true };
+
+  app.use(webpackDevMiddleware(compiler, serverConfig));
+  app.use(webpackHotMiddleware(compiler));
 }
 
 app.get('*', (req, res) => {
-  res.send({ hello: 'express'});
+  res.send(`
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <link type="text/css" rel="stylesheet" href="assets/main.css">
+        <title>Platzi Video</title>
+    </head>
+    <body>
+        <div id="app"></div>
+        <script src="assests/app.js" type="text/javascript"></script>
+    </body>
+    </html>
+  `);
 });
 
 app.listen(PORT, (err) => {
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,13 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-    entry: './src/index.js',
+    entry: ['./src/frontend/index.js', 'webpack-hot-middleware/client?path=/__webpack_hmr&timeout=2000&reload=true'],
+    mode: 'development',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: "bundle.js",
+        filename: "assests/app.js",
         publicPath: '/',
     },
     resolve: {
@@ -56,10 +57,7 @@ module.exports = {
         historyApiFallback: true,
     },
     plugins: [
-        new HtmlWebpackPlugin({
-            template: "./public/index.html",
-            filename: "./index.html"
-        }),
+        new webpack.HotModuleReplacementPlugin(),
         new MiniCssExtractPlugin({
             filename: 'assets/[name].css'
         }),
