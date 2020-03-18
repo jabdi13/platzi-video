651dc8bcddfc99c84f8935ce35ca9788c1bac84c
Abdiel Ortega <nathanabdiel1@gmail.com>
2020-03-18 12:59:33 -0600

    enviroment variables were applied to the project and nodemon

---
+++ src/server/server.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const { ENV, PORT } = process.env;
+const app = express();
+
+if (ENV === 'development') {
+  console.log('Development config');
+}
+
+app.get('*', (req, res) => {
+  res.send({ hello: 'express'});
+});
+
+app.listen(PORT, (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(`Server running on http://localhost:${PORT}`);
+  }
+});
